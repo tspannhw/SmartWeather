5e673154fb4bfe345634a016607d9ad9df157cc8
Timothy Spann <18673814+tspannhw@users.noreply.github.com>
2020-11-13 12:21:01 -0500

    Update kafkatopics.sh

--- kafkatopics.sh
+++ kafkatopics.sh
@@ -1,1 +1,2 @@
 kafka-topics --create --partitions 16 --replication-factor 1 --zookeeper $(hostname -f):2181/kafka --topic flink-weather-alerts
+kafka-topics --create --partitions 16 --replication-factor 1 --zookeeper $(hostname -f):2181/kafka --topic metrics-topic.log
