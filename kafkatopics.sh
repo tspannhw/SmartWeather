462d6dd3d8438ff830e018cade56e6b730f8d25f
Timothy Spann <18673814+tspannhw@users.noreply.github.com>
2020-11-13 12:24:49 -0500

    Update kafkatopics.sh

--- kafkatopics.sh
+++ kafkatopics.sh
@@ -1,2 +1,6 @@
 kafka-topics --create --partitions 16 --replication-factor 1 --zookeeper $(hostname -f):2181/kafka --topic flink-weather-alerts
 kafka-topics --create --partitions 16 --replication-factor 1 --zookeeper $(hostname -f):2181/kafka --topic metrics-topic.log
+
+
+# schema.registry.url=http://edge2ai-1.dim.local:7788/api/v1
+# 7788/swagger
