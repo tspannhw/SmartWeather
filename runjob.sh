df9593a239882b7609d3a8f542006ec4d6698da4
Timothy Spann <18673814+tspannhw@users.noreply.github.com>
2020-11-17 12:14:05 -0500

    Update runjob.sh

--- runjob.sh
+++ runjob.sh
@@ -1,7 +1,7 @@
-flink run --jobmanager yarn-cluster --detached --parallelism 2 --yarnname WeatherApp \
--yt application.properties \
+flink run --jobmanager yarn-cluster --detached --parallelism 1 --yarnname WeatherApp \
+-yt /opt/demo/conf/application.properties \
 weather-1.0.jar \
 --kafka.bootstrap.servers edge2ai-1.dim.local:9092 \
 --kafkaTopic weather \
 --hdfsOutput hdfs:///tmp/weatherapp \
---properties.file application.properties
+--properties.file /opt/demo/conf/application.properties
